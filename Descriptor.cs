52dab6bbadb1facf40438edbe584c53f8a6b3958
Nick Galko <nikolay.galko@gmail.com>
2017-02-13 22:30:23 +0300

    Started Edu/Work Mode manager

---
+++ Descriptor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Descriptor
+    {
+        public double timestart = 0;
+        public double timeend = 1500.0;
+
+        public double h_step = 0.1;
+
+        public double u0_min = 300;
+        public double u0_value = 300;
+        public double u0_max = 1000;
+
+        public double r0_min = 0.1;
+        public double r0_value = 0.1;
+        public double r0_max = 1;
+
+        // Используется только в режиме обучения
+        public double Edu_targetValueD = 0.0;
+        public string Edu_taskDesc = null;
+
+        //TODO: For next-generation of program
+        // you can describe specific coefficients
+
+        public Descriptor
+            (
+            double _timestart, double _timeend,
+            double _h_step,
+            double _u0_min, double _u0_value, double _u0_max,
+            double _r0_min, double _r0_value, double _r0_max,
+
+            double _Edu_targetValueD, string _Edu_taskDesc
+            )
+        {
+            timestart = _timestart;
+            timeend = _timeend;
+            h_step = _h_step;
+            u0_min = _u0_min;
+            u0_value = _u0_value;
+            u0_max = _u0_max;
+            r0_min = _r0_min;
+            r0_value = _r0_value;
+            r0_max = _r0_max;
+
+            // Edu mode
+            Edu_targetValueD = _Edu_targetValueD;
+            Edu_taskDesc = _Edu_taskDesc;
+        }
+    }
+}
---
+++ ModeManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public class ModeManager
+    {
+        enum Edu_Scenario
+        {
+            Edu_Scenario_First = 0,
+            Edu_Scenario_Second,
+            Edu_Scenario_Third,
+            Edu_Scenario_NUM
+        }
+        enum WorkMode
+        {
+            WorkMode_Work = 0,
+            WorkMode_Edu,
+            WorkMode_NUM
+        };
+        WorkMode mWorkMode = WorkMode.WorkMode_Work;
+
+        List<Descriptor> m_EduScenarios = new List<Descriptor>();
+        List<Descriptor> m_WorkScenarios = new List<Descriptor>();
+
+        Descriptor m_DefDescriptor = new Descriptor(0/*timestart*/, 1500.0/*timeend*/, 0.1/*h_step*/,
+                                                    /*u0_min*/300, /*u0_value*/ 300, /*u0_max*/ 1000,
+                                                    /*r0_min*/ 0.1, /*r0_value*/ 0.1, /*r0_max*/ 1,
+                                                    /*Edu_targetValueD*/ 0.0, /*Edu_taskDesc*/ "");
+
+        public ModeManager()
+        {
+            RegisterScenarios();
+        }
+
+        public void RegisterScenarios()
+        {
+            _RegisterWorkScenarios();
+            _RegisterEduScenarios();
+        }
+        private void _RegisterWorkScenarios()
+        {
+            // Первый сценарий
+            m_WorkScenarios.Add(new Descriptor
+                (
+                0/*timestart*/, 1500.0/*timeend*/, 0.1/*h_step*/,
+                /*u0_min*/300, /*u0_value*/ 300, /*u0_max*/ 1000,
+                /*r0_min*/ 0.1, /*r0_value*/ 0.1, /*r0_max*/ 1,
+                /*Edu_targetValueD*/ 0.0, /*Edu_taskDesc*/ "WorkMode"
+                ));
+        }
+        private void _RegisterEduScenarios()
+        {
+            // Первый сценарий
+            m_EduScenarios.Add(new Descriptor
+                (
+                0/*timestart*/, 1500.0/*timeend*/, 0.1/*h_step*/,
+                /*u0_min*/300, /*u0_value*/ 300, /*u0_max*/ 1000,
+                /*r0_min*/ 0.1, /*r0_value*/ 0.1, /*r0_max*/ 1,
+                /*Edu_targetValueD*/ 0.0, /*Edu_taskDesc*/ "EduMode"
+                ));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_type"></param>
+        /// <returns></returns>
+        public  Descriptor GetScenario(int _type)
+        {
+            if (mWorkMode == WorkMode.WorkMode_Work)
+            {
+                if (_type > m_WorkScenarios.Count)
+                {
+                    Debug.Assert(_type > m_WorkScenarios.Count, "Запрошенный сценарий не найден, переключение в стандартный режим");
+                    return m_DefDescriptor;
+                }
+                return m_WorkScenarios[_type];
+            }
+            else if (mWorkMode == WorkMode.WorkMode_Edu)
+            {
+                if (_type > m_EduScenarios.Count)
+                {
+                    Debug.Assert(_type > m_EduScenarios.Count, "Запрошенный сценарий не найден, переключение в стандартный режим");
+                    return m_DefDescriptor;
+                }
+                return m_EduScenarios[_type];
+            }
+            Debug.Fail("Некорректный режим работы - переключение в стандартный режим");
+            return m_DefDescriptor;
+        }
+
+        public void SwitchToEduMode()
+        {
+            mWorkMode = WorkMode.WorkMode_Edu;
+        }
+
+        public void SwitchToWorkMode()
+        {
+            mWorkMode = WorkMode.WorkMode_Work;
+        }
+    }
+}
